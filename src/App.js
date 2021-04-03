738140403c2479bf6a9b548e7453c7be5b00c670
Sid Tickle <32840198+siddtickle@users.noreply.github.com>
2021-04-03 14:24:36 -0400

    Displayed 10 questions from Open Trivia API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=10")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setQuestions(res.results);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {questions.map((question) => (
+        <h1>{question.question}</h1>
+      ))}
     </div>
   );
 }
