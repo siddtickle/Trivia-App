c60567a05f93608cac5137aa6d81ea7b3dd38c91
Sid Tickle <32840198+siddtickle@users.noreply.github.com>
2021-04-03 15:53:23 -0400

    Shuffled answer choices for each question

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Button from "@material-ui/core/button";
 import "./App.css";
 
 function App() {
@@ -8,19 +9,52 @@ function App() {
     fetch("https://opentdb.com/api.php?amount=10")
       .then((res) => res.json())
       .then((res) => {
-        console.log(res);
+        //console.log(res);
         setQuestions(res.results);
       });
   }, []);
+
+  const storeChoices = (question) => {
+    let answerChoices = [];
+
+    answerChoices.push(new Array(question.correct_answer));
+    for (var j = 0; j < question.incorrect_answers.length; j++) {
+      answerChoices.push(question.incorrect_answers[j]);
+    }
+    //console.log(answerChoices);
+    return answerChoices;
+  };
+
+  //Credit for shuffle(): https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  function shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
 
   return (
     <div>
       {questions.map((question) => (
         <div>
-          <h1>{question.question}</h1>
-          <h2>{question.correct_answer}</h2>
-          {question.incorrect_answers.map((incorrectAnswer) => (
-            <h2>{incorrectAnswer}</h2>
+          <h4>{question.question}</h4>
+          {shuffle(storeChoices(question)).map((choice) => (
+            <div>
+              <Button>{choice}</Button>
+            </div>
           ))}
         </div>
       ))}
